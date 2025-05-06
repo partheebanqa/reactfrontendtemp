e2459f3ab7561d92fb5764e19b1ffabbe6b703d5
govindaraj-gif <gopiraj2000gopi@example.com>
2025-05-06 22:14:30 +0530

    ++

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/APIAutomationNew",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  // build: {
-  //   outDir: 'build', 
-  // },
 });
