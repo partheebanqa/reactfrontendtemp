87bba9509fe56adb46ebbdd0676b629c1644b84f
kamal <kamalnath1099@gmail.com>
2025-08-02 22:21:39 +0530

    Execution Reports UI Route Added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -80,7 +80,11 @@ export default function Router() {
           <Route path='/notifications' component={Notifications} />
           <Route path='/settings/account' component={AccountSettingsPage} />
           <Route path='/cicd-configuration' component={CICDConfiguration} />
-          <Route path='/executions' component={ExecutionsNew} />
+          <Route path='/executions' component={ExecutionsNew} /> 
+           <Route path='/executions-reports' component={ExecutionReportsPage} />
+          <Route path='/request-chain-reports' component={RequestChainReport} />
+          <Route path='/test-suite-reports' component={TestSuiteReport} />
+
         </AppLayout>
       )}
       <Route path='/pricing' component={Pricing} />
