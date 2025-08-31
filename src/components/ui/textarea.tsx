2afdaffc56b2bf44b7eaf63a566414ed2f949a05
kamal <ranjith.57neela@gmail.com>
2025-08-31 15:11:07 +0530

    Updated UI changes in Request Chains and Test Suites and add breadcums and sidebar chnages

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 const Textarea = React.forwardRef<
@@ -9,7 +8,9 @@ const Textarea = React.forwardRef<
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-base",
+        "placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus:outline-none focus:border-[#136fb0] focus:ring-0 focus:ring-offset-0",
         className
       )}
       ref={ref}
