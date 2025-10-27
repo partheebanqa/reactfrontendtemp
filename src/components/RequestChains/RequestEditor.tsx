22ee83fab16e502a350d1a021bf71d740cd086ae
Kamalnath S <kamalnath1099@gmail.com>
2025-10-27 21:15:50 +0530

    feat: resolved conflict

--- src/components/RequestChains/RequestEditor.tsx
+++ src/components/RequestChains/RequestEditor.tsx
@@ -2113,15 +2113,6 @@ export function RequestEditor({
                     className='w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent'
                     placeholder='Enter bearer token or use {{tokenVariable}}'
                   />
-                  {/* {(processedRequest.authorization?.token ||
-                    processedRequest.authToken) !==
-                    (request.authorization?.token || request.authToken) && (
-                    <div className='mt-1 px-2 py-1 bg-blue-50 border border-blue-200 rounded text-xs font-mono'>
-                      Processed:{' '}
-                      {processedRequest.authorization?.token ||
-                        processedRequest.authToken}
-                    </div>
-                  )} */}
                 </div>
               </div>
             </div>
