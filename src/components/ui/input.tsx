0b24ba3e9c190d4cd35c41b2edb4825963225c9d
Ranjith <ranjith.r@communn.io>
2026-02-20 23:06:34 +0530

    Update input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,12 +13,13 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           "file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground",
           "placeholder:text-muted-foreground",
           // Focus state → only border color change, no ring/offset
-             "focus:outline-none focus:border-[#136fb0] focus:ring-0 focus:ring-offset-0",
+          "focus:outline-none focus:border-[#136fb0] focus:ring-0 focus:ring-offset-0",
           // Disabled
           "disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         {...props}
+
       />
     )
   }
